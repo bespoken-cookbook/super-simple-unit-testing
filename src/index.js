bf3235f6c2419ac803ddc4fd41261fdf26c592c5
John Kelvie <jpk@bespoken.io>
2018-01-22 11:49:23 -0800

    Clean, running project

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const Alexa = require("alexa-sdk");
+
+exports.handler = function (event, context) {
+    const alexa = Alexa.handler(event, context);
+    // To enable string internationalization (i18n) features, set a resources object.
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
+
+const handlers = {
+    "LaunchRequest": function () {
+        this.emit("SayHello");
+    },
+    "HelloWorldIntent": function () {
+        this.emit("SayHello")
+    },
+    "SayHello": function () {
+        this.response.speak("Hello World!");
+        this.emit(":responseReady");
+    },
+    "AMAZON.HelpIntent": function () {
+        const speechOutput = "This is the Hello World Sample Skill. ";
+        const reprompt = "Say hello, to hear me speak.";
+
+        this.response.speak(speechOutput).listen(reprompt);
+        this.emit(":responseReady");
+    },
+    "AMAZON.CancelIntent": function () {
+        this.response.speak("Goodbye!");
+        this.emit(":responseReady");
+    },
+    "AMAZON.StopIntent": function () {
+        this.response.speak("See you later!");
+        this.emit(":responseReady");
+    }
+};
