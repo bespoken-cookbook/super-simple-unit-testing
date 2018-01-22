c868a76a3c2d40fbe198d5b74e0aeaab008c77f4
John Kelvie <jpk@bespoken.io>
2018-01-22 12:25:57 -0800

    Virtual Alexa tests introduced

--- test/index-test.js
+++ test/index-test.js
@@ -1,6 +1,15 @@
 const expect = require("chai").expect;
+const VirtualAlexa = require("virtual-alexa").VirtualAlexa;
 
 describe("Test the hello world skill", function() {
+    let alexa;
+    beforeEach(function () {
+        alexa = VirtualAlexa.Builder()
+            .handler("src/index.handler") // Lambda function file and name
+            .interactionModelFile("./models/en-US.json")
+            .create();
+    });
+
     it("Should agree that true is true", function() {
         expect(true).to.be.true;
     });
@@ -10,5 +19,21 @@ describe("Test the hello world skill", function() {
             expect(true).to.be.true;
             done();
         }, 500);
+    });
+
+    it("Should launch the skill and get a response", function(done) {
+        alexa.launch().then((result) => {
+            expect(result.response.outputSpeech.ssml).to.exist;
+            expect(result.response.outputSpeech.ssml).to.include("Hello World");
+            done();
+        })
+    });
+
+    it("Should utter hello and get a response", function(done) {
+        alexa.utter("hello").then((result) => {
+            expect(result.response.outputSpeech.ssml).to.exist;
+            expect(result.response.outputSpeech.ssml).to.include("Hello World");
+            done();
+        })
     });
 });
